ad9c0837a48c7ed373879382271418c087fa8896
zhaoyg <zhaoyg@wisevirtue.com>
2024-11-25 17:13:25 +0800

    feat: derive-debug 05-phantom-data

--- debug/src/process_customer_debug.rs
+++ debug/src/process_customer_debug.rs
@@ -2,15 +2,45 @@ use super::CustomDebugInfo;
 use darling::ast;
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{Generics, Meta, MetaNameValue};
+use syn::{GenericParam, Generics, Meta, MetaNameValue};
 
-fn extrat_debug_attribute_value<'a>(
+fn extract_debug_attribute_value<'a>(
     attrs: &mut impl Iterator<Item = &'a syn::Attribute>,
 ) -> Option<&'a syn::Expr> {
     attrs.find_map(|attr| match &attr.meta {
         Meta::NameValue(MetaNameValue { path, value, .. }) if path.is_ident("debug") => Some(value),
         _ => None,
     })
+}
+
+fn extract_type_arguments(ty: &syn::Type) -> Vec<String> {
+    match ty {
+        syn::Type::Path(type_path) => type_path
+            .path
+            .segments
+            .iter()
+            .flat_map(|segment| match &segment.arguments {
+                syn::PathArguments::AngleBracketed(args) => args
+                    .args
+                    .iter()
+                    .filter_map(|arg| {
+                        if let syn::GenericArgument::Type(syn::Type::Path(param_path)) = arg {
+                            param_path
+                                .path
+                                .segments
+                                .last()
+                                .map(|seg| seg.ident.to_string())
+                        } else {
+                            None
+                        }
+                    })
+                    .collect(),
+                _ => vec![],
+            })
+            .collect(),
+
+        _ => vec![],
+    }
 }
 
 pub(crate) fn process(info: CustomDebugInfo) -> TokenStream {
@@ -20,14 +50,18 @@ pub(crate) fn process(info: CustomDebugInfo) -> TokenStream {
         generics,
     } = info
     else {
-        panic!("only support struct!");
+        return syn::Error::new(
+            proc_macro2::Span::call_site(),
+            "CustomeDebug only supports struct!",
+        )
+        .to_compile_error();
     };
 
     let fields_chain = fields.iter().map(|field| {
         let field_name = &field.ident;
-        let debug = extrat_debug_attribute_value(&mut field.attrs.iter());
-        if let Some(debug) = debug {
-            quote! {.field(stringify!(#field_name), &format_args!(#debug,&self.#field_name))}
+        let debug_format = extract_debug_attribute_value(&mut field.attrs.iter());
+        if let Some(debug_format) = debug_format {
+            quote! {.field(stringify!(#field_name), &format_args!(#debug_format,&self.#field_name))}
         } else {
             quote! {.field(stringify!(#field_name), &self.#field_name)}
         }
@@ -39,8 +73,25 @@ pub(crate) fn process(info: CustomDebugInfo) -> TokenStream {
         quote! {}
     } else {
         let bound = params.iter().map(|generic_param| {
-            quote! {
-                #generic_param : std::fmt::Debug,
+            let type_param = match generic_param {
+                GenericParam::Type(type_param) => type_param.ident.to_string(),
+                _ => return quote! {},
+            };
+
+            let field_type = fields.iter().find_map(|field| {
+                if extract_type_arguments(&field.ty).contains(&type_param) {
+                    Some(&field.ty)
+                } else {
+                    None
+                }
+            });
+            match field_type {
+                Some(field_type) => quote! {
+                    #field_type : std::fmt::Debug,
+                },
+                None => quote! {
+                    #generic_param : std::fmt::Debug,
+                },
             }
         });
 
